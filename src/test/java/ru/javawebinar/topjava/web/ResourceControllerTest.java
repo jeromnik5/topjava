fd47f7cb1b3aff8ea8f85ff4b6b9571884f6fea7
jeromnik5 <jeromnik5@yandex.ru>
2017-02-24 08:08:07 +0200

    1.2 ResourceControllerTest for style.css(status and contentType)

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+/**
+ * Created by jerom on 24/02/17.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testResource() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.valueOf("text/css")))
+                .andExpect(status().isOk());
+    }
+}
